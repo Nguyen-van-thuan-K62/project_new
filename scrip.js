6fa2600209da982b10e3dbc2f4e5214d533fb7dc
Nguyen-van-thuan-K62 <nvtdhthuan@gmail.com>
2025-03-10 12:04:05 +0700

    put

--- scrip.js
+++ scrip.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
-    if(window.innerWidth > 820){
+
+    // navbar trôi trên máy tính
+    if(window.innerWidth > 992){
         let navbar = document.getElementById("navbar");
         let lastScrollTop = 0;
         const SCROLL_THRESHOLD = 80;
@@ -21,13 +23,15 @@ document.addEventListener("DOMContentLoaded", function () {
             if (currentScroll === 0) {
                 navbar.removeAttribute("style");
                 navbar.style.backgroundColor = "rgb(138, 170, 138)";
+                navbar.style.height = "40px";
             }
 
             lastScrollTop = currentScroll;
         });
     }
 
-    if(window.innerWidth <= 820){
+    // input_search trôi trên ipad và điện thoại
+    if(window.innerWidth <= 992){
         let search = document.getElementById("div_input_search");
         let lastScrollTop = 0;
         const SCROLL_THRESHOLD = 80;
@@ -53,21 +57,30 @@ document.addEventListener("DOMContentLoaded", function () {
             lastScrollTop = currentScroll;
         });
     }
-
+    
+    // exit navbar điện thoại và ipad
     document.getElementById("exit").addEventListener("click", function() {
         document.getElementById("navbarNav").classList.add("hidden");
     });
-
+    
+    //click navbar điện thoại
     document.getElementById("menuButton").addEventListener("click", function() {
         document.getElementById("navbarNav").classList.remove("hidden");
     });
-
+    
+    //ví trị trang trên máy tính 
     document.querySelectorAll(".nav-link").forEach(item => {
         item.addEventListener("click", function() {
             document.querySelectorAll(".nav-link").forEach(link => link.classList.remove("active"));
             this.classList.add("active");
         });
     });
-
     
+    //tooltip cart của section_card
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl);
+    });
 });
+
+
